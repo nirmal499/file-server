4d6ca79701fa1ef3ba0419acc12129cef9725230
nirmal499 <nbaskey499@gmail.com>
2023-01-03 21:48:27 +0530

    Added premission checker function and write key-value to DB function

--- my_base/include/my_base/base.hpp
+++ my_base/include/my_base/base.hpp
@@ -7,6 +7,10 @@
 // #include <stduuid/uuid.h>
 #include <uuid/uuid.h>
 #include <memory>
+#include <string>
+#include <vector>
+#include <sstream>
+#include<unordered_map>
 
 /* MY_BUFF_SIZE is 4*1024 indicating 4KB used in recvBuffer and sendBuffer */
 /* MY_FILE_BUFF_SIZE is 64*1024 indicating 64KB used in sendFile and recvFile*/
@@ -14,6 +18,10 @@
 
 #define MY_BUFF_SIZE 65536
 #define STR_MAX_SIZE 500
+
+#define CONFIDENTIAL_FILE "/home/nbaskey/Desktop/cpp_projects/cpp_file_server/confidential_files/db_file.txt"
+#define SERVER_STORAGE_FOLDER "/home/nbaskey/Desktop/cpp_projects/cpp_file_server/resources/server/"
+#define CLIENT_STORAGE_FOLDER "/home/nbaskey/Desktop/cpp_projects/cpp_file_server/resources/client/"
 
 namespace base_utility
 {
@@ -30,9 +38,12 @@ namespace base_utility
 
         }
     }
-    
 
     string generate_uuid();
+    vector<string> split(const string &str, char delim);
+
+    int is_permitted(const string &file_name,const string &user_name);
+    void write_to_db(const string &key,const string &value);
 
     void my_write(const int &fd,const uint8_t buffer[],const size_t &bufferSize);
     size_t my_read(const int &fd,uint8_t buffer[],const size_t &bufferSize);
@@ -43,7 +54,7 @@ namespace base_utility
     tuple<size_t,size_t> read_UInt(const int &fd);
     tuple<size_t,array<uint8_t,STR_MAX_SIZE>> read_String(const int &fd);
 
-    size_t sendFile(const int &fd, const string &fileName);
-    size_t recvFile(const int &fd, const string &fileName);
+    size_t sendFile(const int &fd, const string &absolute_path_fileName);
+    size_t recvFile(const int &fd, const string &absolute_path_fileName);
 
 }
--- my_base/src/base.cpp
+++ my_base/src/base.cpp
@@ -7,10 +7,11 @@
 */
 void base_utility::my_write(const int &fd,const uint8_t buffer[],const size_t &bufferSize){
     size_t total_bytes_written = 0;
+    ssize_t bytes_written{};
 
     while (total_bytes_written != bufferSize)
     {
-        ssize_t bytes_written = ::write(fd, &buffer[total_bytes_written],std::min(static_cast<size_t>(MY_BUFF_SIZE),bufferSize - total_bytes_written));
+        bytes_written = ::write(fd, &buffer[total_bytes_written],std::min(static_cast<size_t>(MY_BUFF_SIZE),bufferSize - total_bytes_written));
         base_utility::CHECK(bytes_written,"Bytes written returned -1 in my_write()");
 
         total_bytes_written += bytes_written;
@@ -22,9 +23,10 @@ void base_utility::my_write(const int &fd,const uint8_t buffer[],const size_t &b
 */
 size_t base_utility::my_read(const int &fd,uint8_t buffer[],const size_t &bufferSize){
     size_t total_bytes_read = 0;
+    ssize_t bytes_read{};
 
     while(total_bytes_read != bufferSize){
-        ssize_t bytes_read = ::read(fd, &buffer[total_bytes_read],std::min(static_cast<size_t>(MY_BUFF_SIZE),bufferSize - total_bytes_read));
+        bytes_read = ::read(fd, &buffer[total_bytes_read],std::min(static_cast<size_t>(MY_BUFF_SIZE),bufferSize - total_bytes_read));
         base_utility::CHECK(bytes_read,"Bytes read returned -1 in my_read()");
 
         if(bytes_read == 0){
@@ -61,7 +63,7 @@ std::tuple<size_t,size_t> base_utility::read_UInt(const int &fd){
     auto bytes_read = base_utility::my_read(fd,buffer_for_length.data(),sizeof(size_t));
     
     // https://stackoverflow.com/questions/52492229/c-byte-array-to-int
-    size_t data;
+    size_t data{};
     memcpy(&data,buffer_for_length.data(),sizeof(size_t));
     return std::make_tuple(bytes_read,data);
 }
@@ -98,11 +100,12 @@ std::tuple<size_t,std::array<uint8_t,STR_MAX_SIZE>> base_utility::read_String(co
     return std::make_tuple(bytes_read2,buffer_for_string);
 }
 
-size_t base_utility::sendFile(const int &fd, const string &fileName){
+
+size_t base_utility::sendFile(const int &fd, const string &absolute_path_fileName){
     
     std::array<uint8_t,MY_BUFF_SIZE> buffer;
 
-    ifstream uploadfile(fileName,ios_base::binary);
+    ifstream uploadfile(absolute_path_fileName,ios_base::binary);
 
     if(!uploadfile.is_open()){
         return 0;
@@ -115,9 +118,10 @@ size_t base_utility::sendFile(const int &fd, const string &fileName){
     base_utility::write_UInt(fd,fileSize);
 
     size_t total_bytes_to_be_send = fileSize;
+    size_t to_read_bytes{};
 
     while(total_bytes_to_be_send != 0){
-        size_t to_read_bytes = min(total_bytes_to_be_send,static_cast<size_t>(MY_BUFF_SIZE));
+        to_read_bytes = min(total_bytes_to_be_send,static_cast<size_t>(MY_BUFF_SIZE));
 
         if(!uploadfile.read(reinterpret_cast<char *>(buffer.data()),to_read_bytes)){
             return 0;
@@ -133,10 +137,10 @@ size_t base_utility::sendFile(const int &fd, const string &fileName){
     return fileSize;
 }
 
-size_t base_utility::recvFile(const int &fd, const string &absolute_fileName){
+size_t base_utility::recvFile(const int &fd, const string &absolute_path_fileName){
     std::array<uint8_t,MY_BUFF_SIZE> buffer;
 
-    ofstream outfile(absolute_fileName, ios_base::binary);
+    ofstream outfile(absolute_path_fileName, ios_base::binary);
 
     if(!outfile.is_open()){
         return 0;
@@ -148,9 +152,10 @@ size_t base_utility::recvFile(const int &fd, const string &absolute_fileName){
     }
 
     size_t total_bytes_to_be_read = fileSize;
+    size_t to_write_bytes{};
 
     while(total_bytes_to_be_read != 0){
-        size_t to_write_bytes = base_utility::my_read(fd,buffer.data(),min(static_cast<size_t>(MY_BUFF_SIZE), total_bytes_to_be_read));
+        to_write_bytes = base_utility::my_read(fd,buffer.data(),min(static_cast<size_t>(MY_BUFF_SIZE), total_bytes_to_be_read));
 
         if(to_write_bytes == 0 || !outfile.write(reinterpret_cast<char *>(buffer.data()),to_write_bytes)){
             return 0;
@@ -190,6 +195,84 @@ std::string base_utility::generate_uuid(){
     // Since str_uuid.get() gives a raw pointer so, thats why we need to use '->' instead of '.'
     uuid_unparse_lower(bin_uuid, str_uuid.get()->data());
 
-    std::string my_str_uuid(str_uuid.get()->data());
+    string my_str_uuid(str_uuid.get()->data());
     return my_str_uuid;
 }
+/**
+ * It returns -1 if the internal file was not able to be opened
+ * It returns 0 means the file_name passed does not exists in the DB
+ * It returns 1 means the user_name passed is not permitted to access the file_name
+ * It returns 2 means the user_name passed is permitted to access the file_name
+*/
+int base_utility::is_permitted(const string &file_name,const string &user_name){
+    ifstream infile(CONFIDENTIAL_FILE,ios_base::in);
+
+    if(!infile.is_open()){
+        return -1;
+    }
+
+    string temp_string;
+    // 0 means files does not exists
+    // 1 means it is not permitted
+    // 2 means it is permitted
+    int flag_for_permitted = 1;
+    size_t pos_of_separator{};
+    string key_file_name{};
+    string value_permission_str{};
+    vector<string> permitted_users_vec{};
+
+    while(getline(infile,temp_string)){
+        if(temp_string.empty()){
+            break;
+        }
+
+        pos_of_separator = temp_string.find_first_of("<====>");
+        // pos_of_separator is not included in key_name string
+        key_file_name = temp_string.substr(0,pos_of_separator);
+
+        value_permission_str = temp_string.substr(pos_of_separator+1);
+
+        if(key_file_name == file_name){
+            permitted_users_vec = base_utility::split(value_permission_str,';');
+            if(find(permitted_users_vec.begin(),permitted_users_vec.end(),user_name) != permitted_users_vec.end()){
+                flag_for_permitted = 2;
+                break;
+            }
+        }
+        flag_for_permitted = 0;
+
+    }
+
+    return flag_for_permitted;
+
+}
+
+void base_utility::write_to_db(const std::string &key,const std::string &value){
+    ofstream outfile(CONFIDENTIAL_FILE,ios_base::app);
+
+    if(!outfile.is_open()){
+        throw runtime_error("Error opening the confidential file..\n");
+    }
+
+    string line_to_append = key + "<====>" + value + '\n';
+    outfile.write(line_to_append.data(),line_to_append.size());
+}
+
+
+/***
+ * Returns a vector containing string after splitting the passed string 'str' as per the passed char delimiter 'delim'
+*/
+std::vector<std::string> base_utility::split(const string &str, char delim)
+{
+    vector<string> result{};
+    stringstream ss(str);
+    string item;
+
+    while (getline(ss, item, delim))
+    {
+        result.push_back(item);
+    }
+
+    return result;
+}
+
