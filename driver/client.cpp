7ec0e1d73180fa6313d1610841d64bc80e9fab8c
nirmal499 <nbaskey499@gmail.com>
2023-01-31 23:48:03 +0530

    cmdline arguments will be checked only when required

--- driver/client.cpp
+++ driver/client.cpp
@@ -5,28 +5,27 @@
 int main(int argc, char **argv)
 {
     try
-    {   
+    {
+        client::myclient client{PORT};
+
+#if SERVER_TYPE == 1
+        client.echo();
+#elif SERVER_TYPE == 2
         if (argc < 2)
         {
             std::cout << "Please provide the config file!!!!\n";
             return EXIT_FAILURE;
         }
-
-        client::myclient client{PORT};
-
-    #if SERVER_TYPE == 1
-            client.echo();
-    #elif SERVER_TYPE == 2
-            client.start(argv[1]);
-    #elif SERVER_TYPE == 3
-            client.echo_for_multithreaded_server();
-    #elif SERVER_TYPE == 4
-            client.echo();
-    #elif SERVER_TYPE == 5
-            client.echo();
-    #elif SERVER_TYPE == 6
-            client.echo();
-    #endif
+        client.start(argv[1]);
+#elif SERVER_TYPE == 3
+        client.echo_for_multithreaded_server();
+#elif SERVER_TYPE == 4
+        client.echo();
+#elif SERVER_TYPE == 5
+        client.echo();
+#elif SERVER_TYPE == 6
+        client.echo();
+#endif
 
         return EXIT_SUCCESS;
     }
